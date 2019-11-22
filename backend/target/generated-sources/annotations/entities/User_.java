797147a2f387132bc667b85cef064366e86498ff
David <david.martin.carl@gmail.com>
2019-11-22 19:43:40 +0100

    more playground

--- backend/target/generated-sources/annotations/entities/User_.java
+++ backend/target/generated-sources/annotations/entities/User_.java
@@ -4,11 +4,12 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-20T22:08:35")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-22T19:30:49")
 @StaticMetamodel(User.class)
 public class User_ { 
 
     public static volatile SingularAttribute<User, String> password;
+    public static volatile SingularAttribute<User, String> salt;
     public static volatile SingularAttribute<User, Integer> agencyNumber;
     public static volatile SingularAttribute<User, String> userName;
     public static volatile SingularAttribute<User, Integer> userId;
