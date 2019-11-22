f7587cd0381fd395b4166314feda141904c5c634
HazemSaeid <hazem2670@live.dk>
2019-11-22 22:01:31 +0100

    added mappings.

--- backend/src/main/java/entities/SchemaGenerator.java
+++ backend/src/main/java/entities/SchemaGenerator.java
@@ -11,23 +11,21 @@ public class SchemaGenerator {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("sebastian");
         EntityManager em = emf.createEntityManager();
-    }
-
-    public static void createCountry(EntityManager em, Collection<Country> countries){
-//        Country country = em.getReference(Country.class, 201);
-//        System.out.println(country.getTimeZone().getID());
-
-//        Collection<Country> countries = new ArrayList<Country>();
-//        countries.add(new Country("Denmark", TimeZone.getTimeZone("Europe/Copenhagen")));
-//        countries.add(new Country("Germany", TimeZone.getTimeZone("Europe/Berlin")));
-//        countries.add(new Country("NewYork", TimeZone.getTimeZone("America/New_York")));
-//        createCountry(em, countries);
-
+        Collection<Country> countries = new ArrayList<Country>();
+        countries.add(new Country("Denmark", TimeZone.getTimeZone("Europe/Copenhagen")));
+        countries.add(new Country("Germany", TimeZone.getTimeZone("Europe/Berlin")));
+        countries.add(new Country("NewYork", TimeZone.getTimeZone("America/New_York")));
 
         em.getTransaction().begin();
-        for (Country c: countries) {
+        for (Country c : countries) {
             em.persist(c);
         }
         em.getTransaction().commit();
     }
 }
+
+//    public static void createCountry(EntityManager em, Collection<Country> countries){
+//       Country country = em.getReference(Country.class, 201);
+//        System.out.println(country.getTimeZone().getID());
+
+
--- backend/target/generated-sources/annotations/entities/Flight_.java
+++ backend/target/generated-sources/annotations/entities/Flight_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-22T20:06:16")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-22T21:39:32")
 @StaticMetamodel(Flight.class)
 public class Flight_ { 
 
